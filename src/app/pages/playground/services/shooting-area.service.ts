c15bed116edd243bc500695bf5ba85345711e4a7
Denys Shchablevskyi <dendhg.rgbg@gmail.com>
2024-07-28 14:04:35 +0300

    [4] [FEATURE] implemented mode selection

--- src/app/pages/playground/services/shooting-area.service.ts
+++ src/app/pages/playground/services/shooting-area.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, signal } from '@angular/core';
-import { filter, interval, map, merge, Subject, Subscription, tap } from 'rxjs';
+import { filter, interval, map, merge, startWith, Subscription, tap } from 'rxjs';
 import { ShootingArea } from '../models/shooting-area.model';
-import Difficulty = ShootingArea.Difficulty;
+import Mode = ShootingArea.Mode;
 
 @Injectable()
 export class ShootingAreaService {
-  private readonly _statistics = signal<ShootingArea.Statistics>({ hit: 0, miss: 0 })
+  private readonly _statistics = signal<ShootingArea.Statistics>({ hit: 0, miss: 0, lost: 0 });
   private readonly _stopwatch = signal<number>(0);
   private readonly _difficulty = signal<ShootingArea.Difficulty>(ShootingArea.Difficulty.Easy);
+  private readonly _mode = signal<ShootingArea.Mode>(ShootingArea.Mode.Endless);
+  private readonly _targetScore = signal<number>(0);
+  private readonly _timer = signal<number>(0);
+  private readonly _totalTargets = signal<number>(0)
+
   private readonly _targets$ = signal<ShootingArea.Target[]>([]);
   private readonly _running = signal<boolean>(false);
-  private readonly _newTargetRequest$ = new Subject<void>();
+  private readonly stopAudio = new Audio('assets/finish.wav');
 
   private _sessionSubscription: Subscription | null = null;
   private _shootingArea: SVGElement | null = null;
@@ -35,36 +40,39 @@ export class ShootingAreaService {
     this._shootingArea = svgElement;
   }
 
-  start(difficulty: Difficulty) {
+  start(config: ShootingArea.Config) {
     this.resetStatistics();
     this.resetStopwatch();
-    this._difficulty.set(difficulty);
+    this._totalTargets.set(0);
+    this._difficulty.set(config.difficulty);
+    this._mode.set(config.mode);
     this._running.set(true);
+
+    config.mode === Mode.Timer ? this._timer.set(config.timer) : this._targetScore.set(config.targetScore);
 
     const stopwatch$ = interval(1000)
       .pipe(
         tap(() => this._stopwatch.update(value => value + 1)),
+        filter(() => !!this._timer() && this.stopwatch() === this._timer()),
+        tap(() => this.stop()),
       );
-
-    const targets$ = merge(
-        interval(this.frequency),
-        this._newTargetRequest$
-      )
+    const targets$ = interval(this.frequency)
         .pipe(
-          filter(() => !!this._shootingArea),
+          startWith(0),
           map(() => this.createTarget()),
+          filter(() => this.running()),
           tap((target: ShootingArea.Target) => this._targets$.update(targets => [...targets, target])),
         );
 
     this._sessionSubscription = merge(stopwatch$, targets$).subscribe();
-
-    this._newTargetRequest$.next();
   }
 
   stop() {
     this._running.set(false);
     this._sessionSubscription?.unsubscribe();
     this._targets$().forEach((target) => target.destroy());
+    this.stopAudio.volume = 0.2;
+    this.stopAudio.play();
   }
 
   hitTarget(id: string) {
@@ -79,11 +87,17 @@ export class ShootingAreaService {
   destroyTarget(id: string) {
     this._targets$.update((targets) => targets.filter(target => target.id !== id));
 
+    const { hit, lost } = this.statistics()
+
+    if (this._targetScore() === this._totalTargets() && this._totalTargets() === hit + lost) {
+      this.stop();
+    }
+
     document.getElementById(id)?.remove();
   }
 
   private resetStatistics() {
-    this._statistics.set({ hit: 0, miss: 0 });
+    this._statistics.set({ hit: 0, miss: 0, lost: 0 });
   }
 
   private resetStopwatch() {
@@ -107,13 +121,16 @@ export class ShootingAreaService {
     target.setAttribute('id', id.toString());
     target.style.transition = `${lifetime}ms`;
 
-    this._shootingArea!.appendChild(target);
+    if (this._targetScore() !== this._totalTargets()) {
+      this._shootingArea!.appendChild(target);
+      this._totalTargets.update((total) => total + 1);
+    }
 
     // TODO: avoid setTimeout (need to fix bug with wrong size on first render)
     setTimeout(() => target.style.r = (10).toString(), 100);
 
     const missAfterLifetime = setTimeout(() => {
-      this.missTarget();
+      this._statistics.update((statistics) => ({ ...statistics, lost: statistics.lost + 1 }));
       this.destroyTarget(id);
     }, lifetime)
 
